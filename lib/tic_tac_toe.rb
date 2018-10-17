970259a60874f0c6d4d2d300286a137c026c5408
LOI PHAT DINH <liamphatdinh@gmail.com>
2018-10-17 01:56:09 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -53,7 +53,9 @@ class TicTacToe
 
   def won?
     WIN_COMBINATIONS.any? do |combo|
-      position_taken?(combo[0]) && @board[combo[0]] == @board[combo[1]] && @board[combo[1]] == @board[combo[2]] ? combo : false
+      if position_taken?(combo[0]) && @board[combo[0]] == @board[combo[1]] && @board[combo[1]] == @board[combo[2]]
+        return combo
+      end
     end
   end
 
